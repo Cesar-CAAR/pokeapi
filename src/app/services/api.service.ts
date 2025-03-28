e2e7f5119fe4ee88e229acd6007b71ee3c719e5a
Cesar-CAAR <cesar.andres5242@gmail.com>
2025-03-27 21:41:02 -0700

    Modificando el componente de servicio, p

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,16 +8,21 @@ import { environment } from 'src/environments/environment';
 export class ApiService {
 
   constructor(private http: HttpClient) { }
+  
   // obtener info
   public getByUrl(url: string) {
     return this.http.get(url);
   }
+
   //Usando la enviroment - Nos trae todos los datos desde nuestra url base
   public getPokemons() {
     return this.http.get(`${environment.PokeApiBase}/pokemon`)
   }
+
   //Obtener pokemon por el nombre
   public getPokemonByName(name: string) {
+  
     return this.http.get(`${environment.PokeApiBase}/pokemon/${name}`);
+  
   }
 }
